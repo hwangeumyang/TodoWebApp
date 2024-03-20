18e079b54dcc50bd102ba539b4ee461e74c77946
hgy <hwangeumyang@gmail.com>
2024-03-20 12:32:39 +0900

    service example + controller using service

--- todo-spring-web/src/main/java/org/hgy/todospringweb/controller/TodoController.java
+++ todo-spring-web/src/main/java/org/hgy/todospringweb/controller/TodoController.java
@@ -5,20 +5,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.hgy.todospringweb.dto.TodoDTO;
 import org.hgy.todospringweb.dto.ResponseDTO;
+import org.hgy.todospringweb.service.TodoService;
 import java.util.List;
 import java.util.ArrayList;
 
 @RestController
 @RequestMapping("todo")
 public class TodoController {
+	@Autowired
+	private TodoService service;
 	
 	@GetMapping("/testTodo")
 	public ResponseEntity<?> testTodo() {
-		List<TodoDTO> todoMockList = new ArrayList<>();
-		todoMockList.add(new TodoDTO("id", "msg", true));
-		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(todoMockList).build();
+		String str = service.testService();
+		
+//		List<TodoDTO> todoMockList = new ArrayList<>();
+//		todoMockList.add(new TodoDTO("id", "msg", true));
+//		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(todoMockList).build();
+		List<String> list = new ArrayList<>();
+		list.add(str);
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		
 		return ResponseEntity.ok().body(response);
 	}
---
+++ todo-spring-web/src/main/java/org/hgy/todospringweb/service/TodoService.java
@@ -0,0 +1,12 @@
+package org.hgy.todospringweb.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+	
+	public String testService() {
+		return "Test Service";
+	}
+
+}
