ccbfbdd19e67ada32968c8f709e98a93c0f7c391
HG Y <hwangeumyang@gmail.com>
2024-03-26 18:34:16 +0900

    react todo app 환경설정

--- react-ws/todo-react-app/src/index.js
+++ react-ws/todo-react-app/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; // 리액트를 쓰기 위해 임포트
+import ReactDOM from 'react-dom/client'; // 리액트 DOM을 쓰기 위해 임포트
+import './index.css'; // css import
+import App from './App'; // App 컴포넌트 import
+import reportWebVitals from './reportWebVitals'; // 지금은 무시
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // ReactDOM이 내부의 컴포넌트를 'root'앨리먼트에 render
+//App 컴포넌트 사용 예제
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
