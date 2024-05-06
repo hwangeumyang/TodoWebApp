6e3bd2fd3f766a7a0b805d5c265c172dc3756ac6
CAMAEL-LAPTOP\HGY <hwangeumyang@gmail.com>
2024-05-06 09:03:42 +0900

    프로덕션 배포, https 적용까지

--- react-ws/todo-react-app/src/api-config.js
+++ react-ws/todo-react-app/src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname
 if (hostname==="localhost") {
     backendHost = "http://localhost:8080";
 } else {
-    backendHost = "http://prod-todo-api-service-3.ap-northeast-2.elasticbeanstalk.com";
+    backendHost = "https://api.goldensheep.net";
 }
 
 export const API_BASE_URL = `${backendHost}`;
